397c0d914d829e65fef8f8d08635dc215f24b326
Anton Pawlik <git+noreply@antpaw.org>
2024-12-03 08:56:48 +0100

    day 03

---
+++ day_03/part_2.ts
@@ -0,0 +1,51 @@
+import path from "node:path";
+import { assertEq, assertGreaterThan } from "../utils/assert.ts";
+import { readFile } from "../utils/files.ts";
+
+async function runWithFile(filePath: string): Promise<number> {
+	const content = await readFile(filePath);
+	const donts = content.split("don't()")
+	const dos = [donts.shift()]
+	donts.map((part) => {
+		const a = part.split("do()")
+		a.shift()
+		dos.push(a.join(""))
+
+	})
+	return runMul(dos.join(""));
+}
+
+function runMul(content: string) {
+	const mulStatements = content.split("mul(");
+	let result = 0;
+	for (let i = 0; i < mulStatements.length; i++) {
+		const mulStatement = mulStatements[i];
+		result += mul(mulStatement);
+	}
+	return result;
+}
+
+function mul(mulStatement: string) {
+	const numbers = mulStatement.split(",");
+
+	const x = Number.parseInt(numbers[0]);
+	const y = Number.parseInt(numbers[1]);
+	const rebuild = `${x},${y})`
+
+	if (!mulStatement.startsWith(rebuild)) {
+		return 0;
+	}
+	if (x && y) {
+		return x * y;
+	}
+	return 0;
+}
+
+function buildPath(filePath: string): string {
+	return path.join(import.meta.dirname, filePath);
+}
+
+(async () => {
+	assertEq(await runWithFile(buildPath("./input_simple_2.txt")), 48);
+	assertGreaterThan(await runWithFile(buildPath("./input.txt")), 51830478);
+})();
