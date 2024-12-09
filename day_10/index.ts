f3421b124632a56d02e4f44fd191a1ac15e69890
Anton Pawlik <git+noreply@antpaw.org>
2024-12-09 19:24:09 +0100

    prep day 10

---
+++ day_10/index.ts
@@ -0,0 +1,21 @@
+import path from "node:path";
+import { eq, greaterThan, lowerThan } from "../utils/assert.ts";
+import { readFile, readLines } from "../utils/files.ts";
+
+(async () => {
+	const buildPath = (f: string) => path.join(import.meta.dirname, f);
+
+	eq(await runWithFile(buildPath("./input_simple.txt")), 18);
+	// greaterThan(await runWithFile(buildPath("./input.txt")), 2350);
+})();
+
+async function runWithFile(filePath: string): Promise<number> {
+	const lines = await readLines(filePath);
+
+	let result = 0;
+
+	for (let y = 0; y < lines.length; y++) {
+		result++;
+	}
+	return result;
+}
